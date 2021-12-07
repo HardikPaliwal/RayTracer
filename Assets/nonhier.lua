249ecc874bbe5ac2cce7deb4172647ea3d12f8f6
HardikPaliwal <Hardikpaliwal1999@gmail.com>
2021-12-07 13:26:46 -0500

    images now parsed, normal method no longer used

--- Assets/nonhier.lua
+++ Assets/nonhier.lua
@@ -1,9 +1,9 @@
 -- A simple scene with some miscellaneous geometry.
 
-mat1 = gr.material({0.7, 1.0, 0.7}, {0.5, 0.7, 0.5}, 6.666)
-mat2 = gr.material({0.5, 0.5, 0.5}, {0.5, 0.7, 0.5}, 6.666)
-mat3 = gr.material({1.0, 0.6, 0.1}, {0.5, 0.7, 0.5}, 6.666)
-mat4 = gr.material({0.7, 0.6, 1.0}, {0.5, 0.4, 0.8}, 6.666)
+mat1 = gr.material({0.7, 1.0, 0.7}, {0.5, 0.7, 0.5}, 6.666, "", "")
+mat2 = gr.material({0.5, 0.5, 0.5}, {0.5, 0.7, 0.5}, 6.666, "", "")
+mat3 = gr.material({1.0, 0.6, 0.1}, {0.5, 0.7, 0.5}, 6.666, "", "")
+mat4 = gr.material({0.7, 0.6, 1.0}, {0.5, 0.4, 0.8}, 6.666, "", "")
 
 scene_root = gr.node('root')
 
@@ -41,6 +41,6 @@ scene_root:add_child(steldodec)
 white_light = gr.light({-100.0, 150.0, 400.0}, {0.9, 0.9, 0.9}, {1, 0, 0})
 magenta_light = gr.light({400.0, 100.0, 150.0}, {0.7, 0.0, 0.7}, {1, 0, 0})
 
-gr.render(scene_root, 'nonhier.png', 712, 712,
+gr.render(scene_root, 'nonhier.png', 256, 256,
 	  {0, 0, 800}, {0, 0, -1}, {0, 1, 0}, 50,
 	  {0.3, 0.3, 0.3}, {white_light, magenta_light})
--- Assets/test.lua
+++ Assets/test.lua
@@ -1,12 +1,15 @@
 -- A simple scene with some miscellaneous geometry.
 
-mat1 = gr.material({0.7, 1.0, 0.7}, {0.5, 0.7, 0.5}, 25)
-mat2 = gr.material({0.5, 0.5, 0.5}, {0.5, 0.7, 0.5}, 25)
-mat3 = gr.material({1.0, 0.6, 0.1}, {0.5, 0.7, 0.5}, 25)
-mat4 = gr.material({0.7, 0.6, 1.0}, {0.5, 0.4, 0.8}, 25)
 
-scene_root = gr.node('root')
+red = gr.material({0.366046, 0.037182, 0.041638}, {0, 0, 0}, 0, "", "")
+green = gr.material({0.162928, 0.408903, 0.083375}, {0, 0, 0}, 0, "", "")
+white = gr.material({0.740063, 0.742313, 0.733934}, {0, 0, 0}, 0, "woodgrain.png", "")
+mat1 = gr.material({0.7, 1.0, 0.7}, {0.5, 0.7, 0.5}, 6.666, "", "")
 
+
+scene_root = gr.node('scene')
+-- scene_root:rotate('X', 23)
+-- scene_root:translate(6, 2, 15)
 -- s1 = gr.nh_sphere('s1', {0, 0, -400}, 100)
 -- scene_root:add_child(s1)
 -- s1:set_material(mat1)
@@ -19,28 +22,71 @@ scene_root = gr.node('root')
 -- scene_root:add_child(s3)
 -- s3:set_material(mat2)
 
-b1 = gr.nh_box('b1', {-200, -225, 100}, 100)
-scene_root:add_child(b1)
-b1:set_material(mat4)
-
 -- s4 = gr.nh_sphere('s4', {-100, 25, -300}, 50)
 -- s4 = gr.nh_sphere('s4', {-150, -085, 150}, 30)
 -- scene_root:add_child(s4)
 -- s4:set_material(mat3)
 
--- s5 = gr.nh_sphere('s5', {0, 100, -250}, 25)
+-- s5 = gr.nh_sphere('s5', {0, 0, 10}, 5)
 -- scene_root:add_child(s5)
--- s5:set_material(mat1)
+-- s5:set_material(red)
 
 -- A small stellated dodecahedron.
 
--- steldodec = gr.mesh( 'dodec', 'smstdodeca.obj' )
--- steldodec:set_material(mat3)
--- scene_root:add_child(steldodec)
+steldodec = gr.mesh( 'dodec', 'smstdodeca.obj' )
+steldodec:set_material(red)
+steldodec:translate(1, 2, 10)
+steldodec:scale(0.01, 0.01, 0.01)
+scene_root:add_child(steldodec)
 
-white_light = gr.light({-100.0, 150.0, 400.0}, {0.9, 0.9, 0.9}, {1, 0, 0})
-magenta_light = gr.light({400.0, 100.0, 150.0}, {0.7, 0.0, 0.7}, {1, 0, 0})
 
-gr.render(scene_root, 'test.png', 512, 512,
-	  {0, 0, 800}, {0, 0, -1}, {0, 1, 0}, 50,
-	  {0.3, 0.3, 0.3}, {white_light, magenta_light})
+plane = gr.mesh( 'plane', 'plane.obj' )
+plane:rotate("y", 90)
+plane:translate(-1,-1,5)
+scene_root:add_child(plane)
+plane:set_material(green)
+plane:scale(20, 20, 20)
+
+
+plane2 = gr.mesh( 'plane2', 'plane.obj' )
+plane2:translate(-1,-1,20)
+plane2:rotate("z", 90)
+plane2:rotate("y", 90)
+scene_root:add_child(plane2)
+plane2:set_material(white)
+plane2:scale(20, 20, 20)
+
+plane3 = gr.mesh( 'plane3', 'plane.obj' )
+plane3:translate(5,-1,11)
+plane3:rotate("z", 90)
+plane3:rotate("y", 180)
+scene_root:add_child(plane3)
+plane3:set_material(red)
+plane3:scale(15, 15, 15)
+
+plane4 = gr.mesh( 'plane4', 'plane.obj' )
+plane4:translate(-5,-1,11)
+plane4:rotate("z", 90)
+plane4:rotate("y", 180)
+scene_root:add_child(plane4)
+plane4:set_material(red)
+plane4:scale(15, 15, 15)
+
+-- plane5 = gr.mesh( 'plane5', 'plane.obj' )
+-- plane5:translate(-5,7,11)
+-- plane5:rotate("y", 90)
+-- scene_root:add_child(plane5)
+-- plane5:set_material(red)
+-- plane5:scale(15, 15, 15)
+
+s = gr.sphere('s')
+scene_root:add_child(s)
+s:set_material(green)
+s:translate(0, 0, 8)
+
+white_light = gr.light({1, 8, 0}, {0.9, 0.9, 0.9}, {1, 0, 0})
+
+
+gr.render(scene_root, 'test.png', 200, 200,
+	  {0, 2, 0}, {0, 2, 5}, {0, 1, 0}, 50,
+	  {0.3, 0.3, 0.3}, {white_light})
